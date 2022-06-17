7b99e0e5d4d93c8d894ee963b577af3fb86bb1e7
AdityaDeshmukh2912 <84569301+AdityaDeshmukh2912@users.noreply.github.com>
2022-06-17 17:29:01 +0530

    Add files via upload
    Assignment : Question 2

---
+++ Assignment_2/Assignment_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size");
+            int n = Int32.Parse(Console.ReadLine());
+         
+            var numbers = new string[n];
+            Console.WriteLine("Enter Array Elements : ");
+            for (int i = 0; i < n; i++)
+                numbers[i] = Console.ReadLine();
+
+            
+            File.WriteAllLines(@"C:\Users\deshmukha\OneDrive - Lenze SE\Documents\newFile.txt", numbers);
+
+            string[] numbers1 = System.IO.File.ReadAllLines(@"C:\Users\deshmukha\OneDrive - Lenze SE\Documents\newFile.txt");
+
+            Console.WriteLine("Input Array : {0} ", String.Join(",", numbers));
+
+            Console.WriteLine("Input number to find pairs :");
+            var input = Console.ReadLine();
+
+            int count = 0;
+            
+            foreach(var num in numbers)
+            {
+                if (num.Equals(input))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Answer : " + (count / 2));
+        }
+    }
+}
